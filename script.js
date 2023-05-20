4c34b54a08d8b8582367cab2a73fd24b567ec475
BuggyFord <128647975+BuggyFord@users.noreply.github.com>
2023-05-20 15:12:26 -0500

    Added variables for application

---
+++ script.js
@@ -0,0 +1,7 @@
+var button = document.querySelector(".button");
+var inputValue = document.querySelector(".inputButton");
+var cityName = document.querySelector(".name");
+var desc = document.querySelector(".description");
+var windSpeed = document.querySelector(".wind-speed");
+var temp = document.querySelector(".temp");
+var humid = document.querySelector(".humidity");
